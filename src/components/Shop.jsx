9af5bd67004e2092ebada63df3ee04175eae9c5a
Dubyardd <karnshare66@gmail.com>
2024-09-21 21:05:51 +0700

    sdlkfj

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProductContext } from '../context/ProductContext';
+
 
 const Shop = () => {
+  const { products } = useContext(ProductContext);
+  // console.log(products)
+
+  // get only men's and women's clothing category
+  const filterProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    )
+  })
+  console.log(filterProducts)
   return (
-<section>
-    {/* title */}
-    <h3>Our Products</h3>
-    <hr className='h-[2px] md:w-1/2 max-w-96 bg-gradient-to-l from-transparent via-black to-black mb-24 border-none rounded'/>
-</section>
-)
+    <section className='max-padd-container py-20 bg-[#f8f7f4]'>
+      {/* title */}
+      <h3 className='h3'>Our Products</h3>
+      <hr className='h-[2px] md:w-1/2 max-w-96 bg-gradient-to-l from-transparent via-black to-black mb-24 border-none rounded' />
+      {/* shop container */}
+      <div>
+        Shop
+      </div>
+    </section>
+  )
 }
 
 export default Shop
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Children, createContext, useEffect, useState } from 'react'
+import { json } from 'react-router-dom';
+export const ProductContext = createContext();
 
-const ProductContext = () => {
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  // fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      // console.log(data);
+      setProducts(data)
+    }
+    fetchProducts();
+  }, [])
   return (
-    <div>ProductContext</div>
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
   )
 }
 
-export default ProductContext
+export default ProductProvider
