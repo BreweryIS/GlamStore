9fd8440accadacd0530169e800a94694c9d35fd5
BreweryIS <moogle.fanfare@gmail.com>
2024-09-25 12:28:25 +0700

    Already done

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -7,19 +7,38 @@ const ProductDetails = () => {
   const { id } = useParams();
   const { products } = useContext(ProductContext);
   const { addToCart } = useContext(CartContext);
-  const { image } = products;
 
   // get the single product base on id
   const product = products.find((item) => {
-    return item.id === id;
+    return item.id === parseInt(id);
   });
+  if (!product) {
+    return (
+      <section className="h-screen flexCenter">
+        <h3 className="h3">....Loading</h3>
+      </section>
+    );
+  }
+
+  // destructure product
+  const { title, price, description, image } = product;
+
   return (
-    <section>
+    <section className="max-padd-container py-28 xl:py-32">
       <div>
         {/* container */}
-        <div>
+        <div className="flex flex-col xl:flex-row gap-16">
           {/* image */}
-          <img src={image} alt="productImg" height={255} width={255} />
+          <div className="flex-1 flexCenter">
+            <img src={image} alt="productImg" height={255} width={255} className="object-contain aspect-square p-1"/>
+        </div>
+        {/* text */}
+        <div className="flex-1">
+          <h3 className="h3">{title}</h3>
+          <h5 className="bold-24">${price}</h5>
+          <p className="my-8">{description}</p>
+          <button onClick={()=>addToCart(product,product.id)} className="btn-dark">Add to cart</button>
+          </div>
         </div>
       </div>
     </section>
